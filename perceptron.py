068f369899e4373ffe21c16965058a83e7bc14b0
jorgeluisrocha <jlr435@cornell.edu>
2016-10-22 00:53:39 -0400

    Reformatted perceptron.py into a class and added a function for AND gates.

--- perceptron.py
+++ perceptron.py
@@ -9,28 +9,63 @@ from random import choice
 from numpy import array, dot, random
 from pylab import plot, ylim
 
-unit_step = lambda x: 0 if x < 0 else 1
+class Perceptron:
+    def __init__(self):
+    	## Create random weights for all inputs.
+        self.w      = random.rand(3)
 
-training_data = [
-    (array([0,0,1]), 0),
-    (array([0,1,1]), 1),
-    (array([1,0,1]), 1),
-    (array([1,1,1]), 1),
-]
+        self.errors = []    ## Contain lists of errors
+        self.eta    = 0.2   ## Learning rate
+        self.n      = 100   ## Number of iterations
 
-w = random.rand(3)
+    def ORGate(self):
+        ## Create an lambda function for the step function in a perceptron.
+        unit_step = lambda x: 0 if x < 0 else 1
 
-errors = []
-eta = 0.2
-n = 100
 
-for i in range(n):
-	x, expected = choice(training_data)
-	result      = dot(w, x)
-	error       = expected - unit_step(result)
-	errors.append(error)
-	w          += eta * error * x
+        ## Here is the training data for what an OR gate represents.
+        training_data = [
+            (array([0,0,1]), 0),
+            (array([0,1,1]), 1),
+            (array([1,0,1]), 1),
+            (array([1,1,1]), 1),
+            ]
 
-for x, _ in training_data:
-	result      = dot(x, w)
-	print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
+
+        for i in range(self.n):
+        	x, expected     = choice(training_data)
+        	result          = dot(self.w, x)
+        	error           = expected - unit_step(result)
+        	self.errors.append(error)
+        	self.w         += self.eta * error * x
+
+        for x, _ in training_data:
+        	result          = dot(x, self.w)
+        	print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
+
+    def ANDGate(self):
+    	## Create an lambda function for the step function in a perceptron.
+        unit_step = lambda x: 0 if x < 0 else 1
+
+
+        ## Here is the training data for what an AND gate represents.
+        training_data = [
+            (array([0,0,1]), 0),
+            (array([0,1,1]), 0),
+            (array([1,0,1]), 0),
+            (array([1,1,1]), 1),
+            ]
+
+
+        for i in range(self.n):
+        	x, expected     = choice(training_data)
+        	result          = dot(self.w, x)
+        	error           = expected - unit_step(result)
+        	self.errors.append(error)
+        	self.w         += self.eta * error * x
+
+        for x, _ in training_data:
+        	result          = dot(x, self.w)
+        	print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
+
+
